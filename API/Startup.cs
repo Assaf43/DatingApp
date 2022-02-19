32c177e579869fd55ed7353f0a9ff56253ba8442
Assaf43 <himantz43@gmail.com>
2022-02-19 12:00:36 +0200

    Building a walking skeleton part 2 - client

--- API/Startup.cs
+++ API/Startup.cs
@@ -26,6 +26,11 @@ namespace API
             {
                 option.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(option => option.AddDefaultPolicy(
+                policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                }
+            ));            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +46,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors();
 
             app.UseAuthorization();
 
