dae1ceb12c0750da2ac6f65c890a05f732909b1f
Assaf Manzoor <himantz43@gmail.com>
2022-05-02 16:29:52 +0300

    Reactive forms

--- API/Dtos/UserDto.cs
+++ API/Dtos/UserDto.cs
@@ -10,5 +10,6 @@ namespace API.Dtos
         public string UserName { get; set; }
         public string token { get; set; }
         public string PhotoUrl { get; set; }
+        public string KnownAs { get; set; }
     }
 }
